6082a9fc2b95ca620610842ccb585cd2f4e7b8a7
BMWdejf <sablatura.david@gmail.com>
2024-05-11 13:28:53 +0200

    Create services

--- products/services/base.py
+++ products/services/base.py
@@ -1,33 +1,15 @@
 import requests
 from os import getenv
-from celery import shared_task
-import logging
 
-# Configure logging
-logging.basicConfig(level=logging.ERROR)
-
-@shared_task
-def fetch_data_from_flexibee_api():
-
-    # Start from id
-    id_value = 1
-    # Fetch data from Flexibee API
+def fetch_data_from_flexibee_api(start_id=1, end_id=100):
     data_list = []
-
-    while True:
-        url = f"https://sas-technologi.flexibee.eu:5434/c/einteriors_s_r_o_/cenik/{id_value}.json?detail=custom:id,kod,nazev,exportNaEshop,prilohy(nazSoub,%20content,%20link,%20typK)&relations=prilohy"
+    for id in range(start_id, end_id + 1):
+        url = f"https://sas-technologi.flexibee.eu:5434/c/einteriors_s_r_o_/cenik/{id}.json?detail=custom:id,kod,nazev,exportNaEshop,prilohy(nazSoub,%20content,%20link,%20typK)&relations=prilohy"
         auth = (getenv("FLEXB_USER"), getenv("FLEXB_PASS"))
-        try:
-            response = requests.get(url, auth=auth)
-            if response.status_code != 200:
-                logging.error(f"Failed to fetch data from Flexibee API: {response.status_code}")
-                break
+        response = requests.get(url, auth=auth)
+        if response.status_code == 200:
             data = response.json()['winstrom']['cenik']
             data_list.extend(data)
-        except requests.exceptions.RequestException as e:
-            logging.error(f"Error fetching data from Flexibee API: {e}")
-        except KeyError as e:
-            logging.error(f"Error parsing data from Flexibee API: {e}")
-        id_value += 1
-
+        else:
+            print(f"Failed to fetch data from API for ID: {id}")
     return data_list
--- products/services/create.py
+++ products/services/create.py
@@ -1,26 +1,18 @@
 from .base import fetch_data_from_flexibee_api
 from products.models import Products
-import logging
 
-# Configure logging
-logging.basicConfig(level=logging.ERROR)
+def create_products_from_api(start_id=1, end_id=100):
+    data_list = fetch_data_from_flexibee_api(start_id, end_id)
+    for row in data_list:
+        prilohy_link = None
+        if 'prilohy' in row and row['prilohy']:
+            prilohy_link = row['prilohy'][0]['link']
 
-# Create products from Flexibee API
-def create_products_from_api():
-    try:
-        data_list = fetch_data_from_flexibee_api()
-        for row in data_list:
-            prilohy_link = None
-            if 'prilohy' in row and row['prilohy']:
-                prilohy_link = row['prilohy'][0]['link']
-
-            Products.objects.create(
-                fx_id=row['id'],
-                code=row['kod'],
-                name=row['nazev'],
-                img_link=prilohy_link,
-                export_on_eshop=row.get('export_on_eshop', False)
-            )
-            logging.info(f"Product with ID {row['id']} created")
-    except Exception as e:
-        logging.error(f"Error creating products from API: {e}")
+        Products.objects.create(
+            fx_id=row['id'],
+            code=row['kod'],
+            name=row['nazev'],
+            img_link=prilohy_link,
+            export_on_eshop=row.get('export_on_eshop', False)
+        )
+        print(f"Product with ID {row['id']} created")
