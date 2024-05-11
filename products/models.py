4e601d08dad9e533591e36072bddb7323a6f8bc6
BMWdejf <sablatura.david@gmail.com>
2024-05-11 11:55:47 +0200

    Rename column exportNaEshop

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ class Products(models.Model):
     code = models.CharField(max_length=150)
     name = models.CharField(max_length=255)
     img_link = models.CharField(max_length=255)
-    exportNaEshop = models.BooleanField(default=False)
+    export_on_eshop = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
