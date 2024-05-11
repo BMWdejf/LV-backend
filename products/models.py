778f5d75a8d2fb4e3b971e7a3ba4180127f071e4
BMWdejf <sablatura.david@gmail.com>
2024-05-11 11:33:11 +0200

    Add new models for products from flexibee

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    code = models.CharField(max_length=150)
+    name = models.CharField(max_length=255)
+    img_link = models.CharField(max_length=255)
+    exportNaEshop = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
