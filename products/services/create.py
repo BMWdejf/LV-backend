efb0b6344f9de6b6ab629d6414e5daaefd944737
BMWdejf <sablatura.david@gmail.com>
2024-05-12 16:08:06 +0200

    Import new products to database

--- products/services/create.py
+++ products/services/create.py
@@ -1,26 +1,34 @@
-from .base import fetch_data_from_flexibee_api
+#from .base import fetch_data_from_flexibee_api
 from products.models import Products
 import logging
 
 # Configure logging
 logging.basicConfig(level=logging.ERROR)
 
-# Create products from Flexibee API
-def create_products_from_api():
-    try:
-        data_list = fetch_data_from_flexibee_api()
-        for row in data_list:
-            prilohy_link = None
-            if 'prilohy' in row and row['prilohy']:
-                prilohy_link = row['prilohy'][0]['link']
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
 
-            Products.objects.create(
-                fx_id=row['id'],
-                code=row['kod'],
-                name=row['nazev'],
-                img_link=prilohy_link,
-                export_on_eshop=row.get('export_on_eshop', False)
-            )
-            logging.info(f"Product with ID {row['id']} created")
+# Create products from Flexibee API
+def create_product(row):
+    try:
+        prilohy_link = None
+        if 'prilohy' in row and row['prilohy']:
+            prilohy_link = row['prilohy'][0]['link']
+
+        export_na_eshop = row.get('exportNaEshop')
+
+        if export_na_eshop:
+            export_na_eshop = str2bool(export_na_eshop)
+        else:
+            export_na_eshop = False
+
+        Products.objects.create(
+            fx_id=row['id'],
+            code=row['kod'],
+            name=row['nazev'],
+            img_link=prilohy_link,
+            export_on_eshop=export_na_eshop,
+        )
+        logging.info(f"Product with ID {row['id']} created")
     except Exception as e:
         logging.error(f"Error creating products from API: {e}")
