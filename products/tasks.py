0e14a33587de80e8dcc9e3344a3021800f135721
BMWdejf <sablatura.david@gmail.com>
2024-05-12 09:44:21 +0200

    Create tasks celery file

---
+++ products/tasks.py
@@ -0,0 +1,19 @@
+from core.celery import shared_task
+from django.core.mail import send_mail
+
+
+@shared_task
+def task_done(send_to):
+    subject = (
+        "David Šablatura sablatura.david@gmail.com"
+    )
+    message = "This email is from Django. Hello Medium. How are you?"
+
+    email_sent = send_mail(
+        subject=subject,
+        message=message,
+        from_email=None,
+        recipient_list=[send_to]
+    )
+
+    return email_sent
