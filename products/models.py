ec333e32ae14a5a9b4726865546437db8e07362a
BMWdejf <sablatura.david@gmail.com>
2024-05-12 14:11:38 +0200

    Add new column fx_id and setting code how unique=True

--- products/models.py
+++ products/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Products(models.Model):
-    code = models.CharField(max_length=150)
+    fx_id = models.IntegerField(unique=True)
+    code = models.CharField(max_length=150, unique=True)
     name = models.CharField(max_length=255)
     img_link = models.CharField(max_length=255)
     export_on_eshop = models.BooleanField(default=False)
