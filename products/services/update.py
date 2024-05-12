b4e8cc72c77e4f13a73255a903d7d98d13122b31
BMWdejf <sablatura.david@gmail.com>
2024-05-12 09:34:13 +0200

    Modify update.py - add logging library and error catching

--- products/services/update.py
+++ products/services/update.py
@@ -1,20 +1,26 @@
 from .base import fetch_data_from_flexibee_api
 from products.models import Products
+import logging
 
-def update_products_from_api(start_id=1, end_id=100):
-    data_list = fetch_data_from_flexibee_api(start_id, end_id)
-    for row in data_list:
-        prilohy_link = None
-        if 'prilohy' in row and row['prilohy']:
-            prilohy_link = row['prilohy'][0]['link']
+# Configure logging
+logging.basicConfig(level=logging.ERROR)
 
-        Products.objects.update_or_create(
-            fx_id=row['id'],
-            defaults={
-                'code': row['kod'],
-                'name': row['nazev'],
-                'img_link': prilohy_link,
-                'export_on_eshop': row.get('export_on_eshop', False)
-            }
-        )
-        print(f"Product with ID {row['id']} updated or created")
+# Create products from Flexibee API
+def update_products_from_api():
+    try:
+        data_list = fetch_data_from_flexibee_api()
+        for row in data_list:
+            prilohy_link = None
+            if 'prilohy' in row and row['prilohy']:
+                prilohy_link = row['prilohy'][0]['link']
+
+            Products.objects.update(
+                fx_id=row['id'],
+                code=row['kod'],
+                name=row['nazev'],
+                img_link=prilohy_link,
+                export_on_eshop=row.get('export_on_eshop', False)
+            )
+            logging.info(f"Product with ID {row['id']} created")
+    except Exception as e:
+        logging.error(f"Error creating products from API: {e}")
