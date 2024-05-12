2b439d7e2968476f3b92acc922e467469ec1b321
BMWdejf <sablatura.david@gmail.com>
2024-05-12 16:22:32 +0200

    Modify task.py

--- products/tasks.py
+++ products/tasks.py
@@ -1,19 +1,38 @@
 from celery import shared_task
-from django.core.mail import send_mail
+from celery.utils.log import get_task_logger
+import requests
+from os import getenv
+from django.db import transaction
+from products.models import Products
+from products.services.create import create_product
+from products.services.update import update_products
+
+logger = get_task_logger(__name__)
 
 
 @shared_task
-def task_done(send_to):
-    subject = (
-        "David Šablatura sablatura.david@gmail.com"
-    )
-    message = "This email is from Django. Hello Medium. How are you?"
+def fetch_data_from_flexibee_api(url):
+    auth = (getenv("FLEXB_USER"), getenv("FLEXB_PASS"))
+    try:
+        response = requests.get(url, auth=auth)
+        logger.info(response.status_code)
+        if response.status_code == 200:
+            try:
+                with transaction.atomic():
+                    data = response.json()['winstrom']['cenik']
+                    for item in data:
+                        db_item = Products.objects.filter(fx_id=item["id"]).first()
+                        if db_item:
+                            update_products(item)
+                        else:
+                            create_product(item)
 
-    email_sent = send_mail(
-        subject=subject,
-        message=message,
-        from_email=None,
-        recipient_list=[send_to]
-    )
+            except Exception as e:
+                #TODO: Add error logging to file or database
+                pass
+        else:
+            #TODO: Add error logging to file or database
+            logger.error(f"Failed to fetch data from Flexibee API: {response.status_code}")
 
-    return email_sent
+    except (KeyError, requests.exceptions.RequestException) as e:
+        print(e)
